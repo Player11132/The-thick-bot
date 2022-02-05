2846de8a65d99d1961d97bc38652ecfae9452bc1
Player11132 <61601920+Player11132@users.noreply.github.com>
2022-02-05 15:34:06 +0200

    Fixed songs

--- BOI.py
+++ BOI.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 #Libraries
+from discord.ext.commands import Bot
 import discord
 import requests
 import wikipedia
@@ -11,6 +12,10 @@ from datetime import datetime
 import random
 import os
 import ffmpeg
+from discord.utils import get 
+from discord import FFmpegPCMAudio
+import asyncio
+from difflib import SequenceMatcher
 
 # if the bot doesn't run be sure the config.json is in the resources folder named config and with the .json extension
 with open("Resources/config.json","r") as f:
@@ -22,7 +27,7 @@ hostid = config["HostID"]
 
 url = ''
 
-path = []
+urls = []
 index = 0
 name = ''
 
@@ -124,14 +129,25 @@ def generate_youtube_embed(video_data : dict):
 
 @bot.command(brief="it commits wikipedia")
 async def wiki(ctx:commands.Context,*,keyword:str):
-    page = wikipedia.search(keyword,1)
-    if not len(page): await ctx.send(f"No results for `{keyword}`")
+    search = keyword
+    suggestion = wikipedia.suggest(keyword)
+    if SequenceMatcher(None, keyword, suggestion).ratio()>0.5 and not suggestion==None:
+        search = suggestion
+        print("Suggestion more acurate")
     else:
-        # try to send the summary, if there is an ambiguous case, pick the first page
-        try:
-            await ctx.send(wikipedia.summary(page))
-        except wikipedia.exceptions.DisambiguationError as e:
-            await ctx.send(wikipedia.page(e.options[0]).summary)
+        search = wikipedia.search(search,1)
+
+    print(suggestion,search,keyword,wikipedia.search(search,1))
+
+ # try to send the summary, if there is an ambiguous case, pick the first page
+    try:
+        if len(wikipedia.summary(search))<=2000:
+            await ctx.send(wikipedia.summary(search))
+        else:
+            await ctx.send(wikipedia.summary(search,10))
+            await ctx.send(f"For more information visit:\n{wikipedia.page(search).url}")
+    except wikipedia.exceptions.DisambiguationError as e:
+        await ctx.send(wikipedia.page(e.options[0]).summary)
 
 @bot.command(brief="Credits")
 async def Credits(ctx:commands.Context):
@@ -144,7 +160,6 @@ async def Credits(ctx:commands.Context):
 
     embed.add_field(name="Player11132#7328", value="Programmer of the bot itself",inline=False)
     embed.add_field(name="dogerish#1469", value="Absolute chad ,host of the Bot and helper\n(couldn't do it without him)",inline=False)
-    embed.add_field(name="Hey,psst!Do you want Boi?", value="Go to: player11132.github.io",inline=False)
     embed.add_field(name="Thank you for using", value="BOI the amazing discord bot", inline=False)
     await ctx.send(embed=embed)
 
@@ -174,7 +189,7 @@ async def steam_info_id(ctx : commands.Context, id : str):
             description=game_data["short_description"]
         )
     else:
-        await ctx.send("enter valid ID,make sure there are only numbers")
+        await ctx.send("Enter valid ID,make sure there are only numbers")
 
     embed.set_image(url=game_data["header_image"])
     embed.set_footer(text="Steam API", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/200px-Steam_icon_logo.svg.png")
@@ -239,117 +254,130 @@ def youtubelinkgen(keyword):
     else:
         return(False)
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    
-}   
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'} 
+
+FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 #  --Audio commands and logic
 
-@bot.command(brief="Plays the audio of the video you searched,supports link")
-async def play(ctx : commands.Context, *, keyword : str):
-    if not ctx.voice_client or not ctx.voice_client.is_connected():
-        await ctx.send('Not connected to a voice channel,connecting...')
-        if ctx.voice_client and ctx.voice_client.is_connected():
-            await ctx.send('I am already connected')
-
-        channel = ctx.author.voice.channel
-        if channel:
-            await channel.connect()
-        else:
-            await ctx.send('You are not connected to a voice channel')
-            return
+async def play_next(ctx):
+    print(len(urls))
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    urls.pop(0)
+    print(len(urls))
+    if len(urls) > 0:
+        await playsong(ctx)
+    else:
+        await ctx.send("Queue ended")
+        await voice.disconnect()
     
+async def playsong(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    await ctx.send(f"Playing:{urls[0]['title']}")
+    voice.play(FFmpegPCMAudio(urls[0]['url'], **FFMPEG_OPTIONS),after= lambda e:bot.loop.create_task(play_next(ctx)))
+
+@bot.command(brief="Plays requested song")
+async def play(ctx:commands.Context,*,keyword:str):
+    if not ctx.voice_client or not ctx.voice_client.is_connected():
+        await ctx.send("Connecting to voice channel...")
+        if(ctx.message.author.voice is not None):
+            channel = ctx.message.author.voice.channel
+            voice = get(bot.voice_clients, guild=ctx.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
+        else:
+            await ctx.send("You must be connected to a voice channel in order to use the play command")
+            return
+
     if ctx.voice_client.is_playing():
-        await ctx.send('Song already playing, adding song to the queue')
-        if(youtubelinkgen(keyword)!=False):
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                file = ydl.extract_info(url, download=True)
-                path.append(str(file['title']) + "-" + str(file['id'] + ".mp3"))
-                await ctx.send('Song successfully added to the queue')
-                print(path)
-        else:
-            await ctx.send("The search returned no results.")
-    else:
-        with youtube_dl.YoutubeDL({"quiet": True}) as ydl:
+        await ctx.send("Already playing, adding song to the queue")
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             if(youtubelinkgen(keyword)!=False):
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    file = ydl.extract_info(url, download=True)
-                    path.append(str(file['title']) + "-" + str(file['id'] + ".mp3"))
-                    global name
-                    name = str(file['title']) + "-" + str(file['id'] + ".mp3")
+                info = ydl.extract_info(url, download=False)
+                if info not in urls:
+                    urls.append(info)
+                    await ctx.send(f"Song {info['title']} was succsessfully added to the queue \n {url}")
+                else:
+                    await ctx.send(f"Song {info['title']} is already in the queue")
+            else:
+                await ctx.send("Couldn't find song, try another keyword")
+            return
+    else:
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            if(youtubelinkgen(keyword)!=False):
+                info = ydl.extract_info(url, download=False)
+                urls.append(info)
+            else:
+                await ctx.send("Couldn't find song, try another keyword")
+                return
+        await playsong(ctx)
 
-                await ctx.send('Now playing:'+path[0].rstrip('.mp3'))
-                ctx.voice_client.play(discord.FFmpegPCMAudio(path[0]))
-                print(path)
-                
-
-
-@bot.command(brief="If the music stopped use this command")
+@bot.command  (brief="Plays next song in queue")
 async def playnext(ctx:commands.Context):
-    if len(path)!=0:
-        if ctx.voice_client.is_playing():
-            await ctx.send("Already playing! \n Use BOI skip to skip this song.")
-        else:
-            temp = path[0]
-            path.pop(0)
-            ctx.voice_client.play(discord.FFmpegPCMAudio(path[0]))
-            await ctx.send('Now playing:'+path[0].rstrip('.mp3'))
-            os.remove(temp)
-    else:
-        await ctx.send("Queue empty, add songs using the BOI play command")
+    if ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+    bot.loop.create_task(play_next(ctx))
 
-@bot.command(brief="Pauses currently playing song")
-async def pause(ctx : commands.Context):
-    if not ctx.voice_client or not ctx.voice_client.is_connected():
-        await ctx.send('Not in a voice channel')
-        return
-    ctx.voice_client.pause()
+#@bot.command()
+#async def did_you_pay_your_taxes(ctx:commands.Context):
+#    await ctx.send("Taxes, what taxes?")
 
-@bot.command(brief="Skips current song")
-async def skip(ctx: commands.Context):
-    if not ctx.voice_client or not ctx.voice_client.is_connected():
-        await ctx.send('Not in a voice channel')
-        return
-    ctx.voice_client.stop()
-    if(len(path)!=0):
-        temp = path[0]
-        path.pop(0)
-        ctx.voice_client.play(discord.FFmpegPCMAudio(path[0]))
-        await ctx.send("Skipped!")
-        await ctx.send('Now playing:'+path[0].rstrip('.mp3'))
-        os.remove(temp)
-    else:
-        await ctx.send("Queue empty")
+
+
+#@bot.command()
+#async def you_are_commiting_taxfraud(ctx:commands.Context):
+#    await ctx.send("What is tax- \n THUMP THUMP THUMP \n Wtf? \n FBI OPEN UP \n AH FUCK THEY FOUND ME! \n PUT YO HAND IN THE AIR! \n NO FUCK YOU! \n YOU WILL NEVER TAKE ME ALIVE! \n *jumps from window and fucking dies*")
 
 @bot.command(brief="Shows queue")
-async def queue(ctx : commands.Context):
-    if(len(path)!=0):
-        for i in range(len(path)):
-            await ctx.send(str(i+1)+" "+str(path[i].rstrip('.mp3')))
-    else:
+async def queue(ctx:commands.Context):
+    if len(urls)-1==0:
         await ctx.send("Queue empty")
-
-@bot.command(brief="Resumes the last downloaded song")
-async def resume(ctx : commands.Context):
-    if not ctx.voice_client or not ctx.voice_client.is_connected():
-        await ctx.send('Not in a voice channel')
-        return
-    ctx.voice_client.resume()
-
-@bot.command(brief="Leaves voice channel you are in , if its in it")
-async def leave(ctx : commands.Context):
-    if ctx.voice_client and ctx.voice_client.is_connected():
         await ctx.voice_client.disconnect()
-    else:
-        await ctx.send('I am not connected to a voice channel')
+        return
 
-#Shhhhhhhhh!
+    for i in range(len(urls)):
+        if i==0:
+            await ctx.send(f"Playing now: {urls[i]['title']} \n In queue: \n")
+        else:
+            await ctx.send(str(i) + f". {urls[i]['title']}")
+
+@bot.command(brief="Leaves voice chat")
+async def leave(ctx:commands.Context):
+    if not ctx.voice_client or not ctx.voice_client.is_connected():
+        await ctx.send("Not in any voice chat")
+        return
+    await ctx.send("Leaving voice chat")
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    await voice.disconnect()
+    urls = []
+
+@bot.command(brief="Pauses music")
+async def pause(ctx:commands.Context):
+    if not ctx.voice_client or not ctx.voice_client.is_connected():
+        await ctx.send("Not in voice chat")
+        return
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send("Music Paused")
+    else:
+        await ctx.send("Not playing any music")
+
+@bot.command(breif="Resumes music")
+async def resume(ctx:commands.Context):
+    if not ctx.voice_client or not ctx.voice_client.is_connected():
+        await ctx.send("Not in voice chat")
+        return
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        await ctx.send("Alredy playing music")
+    else:
+        voice.resume()
+        await ctx.send("Music resumed")
+
 @bot.command(brief="well , its in the name")
 async def rickroll(ctx:commands.Context):
     if not ctx.voice_client or not ctx.voice_client.is_connected():
